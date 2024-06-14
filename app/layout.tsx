6afd8bd3d65f30af6ef3c44c40bad67624b9693e
Harsh Saini <harshsaini100@gmail.com>
2024-06-14 22:17:35 +0530

    First Commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import Sidebar from "./components/Sidebar";
+import "./globals.css";
+
+const figtree = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Spotify Clone",
+  description: "Listen to music!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={figtree.className}>
+      <Sidebar>
+        {children}
+      </Sidebar>      
+      </body>
+    </html>
+  );
+}
